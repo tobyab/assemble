618e66992f17d2d8a7aadfa55137131c7cf45b7f
Toby <mrtobez08@gmail.com>
2022-08-07 08:10:37 -0700

    changes made to webhook and stuff

--- pages/inspire.tsx
+++ pages/inspire.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Inspire.module.css'
+import useSWR, { useSWRConfig } from "swr"
+import fetcher from '../utils/fetch'
 
 export default function Inspire() {
     const randInt = Math.floor(Math.random() * 5) + 1
+
+    const { mutate } = useSWRConfig()
+    const { data } = useSWR<{
+        body?: string,
+        author?: string
+    }>("/api/quote" + fetcher)
+    
     return (
         <div className="absolute">
             <div className={styles.content}>
@@ -11,8 +20,14 @@ export default function Inspire() {
                     src={`/bg/${randInt}.jpg`}
                 />
                 <div className="absolute top-32 pl-64">
-                    <h1 className="text-9xl font-bold text-white w-2/3">&qout;i hate this font.&qout;</h1>
-                    <p className="text-white text-2xl">- Slack user</p>
+                    {data ? (
+                        <>
+                            <h1 className="text-9xl font-bold text-white w-2/3">{data.body}</h1>
+                            <p className="text-white text-2xl">- {data.author}</p>
+                        </>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
                 </div>
             </div>
         </div>
