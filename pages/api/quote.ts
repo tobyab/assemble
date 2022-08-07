f04cb0c536521e4852759c5110e53b161bd4f022
Toby <mrtobez08@gmail.com>
2022-08-06 20:46:59 -0700

    changes made

--- pages/api/quote.ts
+++ pages/api/quote.ts
@@ -2,42 +2,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../utils/prisma'
 
-export default async function handler(
+export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     const funnyQuotes = await prisma.quote.findMany();
     return res.json(
       funnyQuotes.map((quote) => ({
         body: quote.body,
         author: quote.author,
-        updated_at: quote.updated_at
       }))
       )
     }
 
-  if(req.method === "POST") {
+  const { body, author } = req.body;
+  if (req.method === "POST") {
     const newData = await prisma.quote.create({
       data: {
-        body,
-        author
+        body: body,
+        author: author
       }
     })
-    return res.status(200).json({
-      body: newData.body,
-      author: newData.author
-    })
+    res.json(newData)
+    } else {
+      res.status(400).json({ error: "Invalid request" })
+    }
   }
-  }
-
-  /*
-    return res.json(
-      quote.map((entry) => ({
-        id: entry.id.toString(),
-        body: entry.body,
-        created_by: entry.created_by,
-        updated_at: entry.updated_at
-      }))
-    );
-    */
